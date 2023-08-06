c04ea66e10895593a5ca309825c66df86cb20261
Andreas Chiocchetti <andreas.chiocchetti@kgu.de>
2023-08-06 06:21:18 +0000

    update geneviewer

--- geneviewer/app.R
+++ geneviewer/app.R
@@ -185,10 +185,10 @@ metadata=samplemeta
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-  
+
   # Application title
   titlePanel("Gene viewer scRNA"),
-  
+
   # Sidebar with a slider input for number of bins
   fluidRow(
     column(2,
@@ -198,16 +198,11 @@ ui <- fluidPage(
                           unique(rownames(counts)),
                           selected="Gapdh",
                           multiple = T), p("paste multiple genes with space as delimiter")),
-    
+
     column(2,
            selectInput("cluster", "Cell cluster:",
                        sort(as.character(unique(metadata$Celltype))),
-                       selected=c("Microglia_0",
-                                  "Microglia_1",
-                                  "Microglia_2",
-                                  "Microglia_3",
-                                  "Microglia_4",
-                                  "Microglia_5"),
+                       selected=unique(metadata$Celltype),
                        multiple = T)),
     column(2,
            selectInput("treatment", "Condition",
@@ -238,10 +233,10 @@ ui <- fluidPage(
                          plotOutput("distPlot")),
                 tabPanel("Stats",
                          DT::dataTableOutput("distTable")),
-                
+
                 tabPanel(title="Gene Maps",
                          plotOutput("MapPlots")),
-                
+
                 tabPanel(title="Feature Maps",
                          selectInput("condition",
                                      "Feature",multiple=F,
@@ -262,15 +257,16 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   ## gene plot
-  
+
   observe({
     req(input$gene)
     req(input$cluster)
     req(input$treatment)
-    filteridx <-  metadata$Celltype %in% input$cluster & metadata$corrGenotype_Treatment %in% input$treatment
+    filteridx <-  which(metadata$Celltype %in% input$cluster & metadata$corrGenotype_Treatment %in% input$treatment)
+
     plotdata <- counts[input$gene, filteridx]
-    plotdata <- rbind(plotdata, t(metadata[filteridx,]))
-    
+    plotdata <- rbind(plotdata, t(metadata[colnames(plotdata),]))
+
     plotdata <- t(plotdata) %>%  as.data.frame()
     plotdata <- plotdata %>% mutate_at(input$gene,as.numeric)
     if(input$scale == "yes"){
@@ -281,16 +277,16 @@ server <- function(input, output) {
                                             variable.name="gene",
                                             measure.vars = input$gene)
     plotdata$counts=plotdata$counts %>% as.numeric()
-    
+
     plotdata$corrGenotype_Treatment=paste0(plotdata$Genotype_corr," ", plotdata$Treatment)
-    
-    
-    if(input$switch == "bygene"){ 
+
+
+    if(input$switch == "bygene"){
       if(length(input$treatment)>1){
         ggpubr:::compare_means(counts ~ corrGenotype_Treatment,
                                data = plotdata, method = "wilcox.test",
                                group.by = "gene")
-        
+
         gp<-ggplot(plotdata, aes(x=corrGenotype_Treatment, y=counts, fill=corrGenotype_Treatment)) +
           geom_boxplot(outlier.shape = NA)+geom_jitter(size=1,width=0.05, aes(colour = methoxy))+
           facet_grid(cols=vars(gene), rows=vars(Celltype), scales="free") +
@@ -298,7 +294,7 @@ server <- function(input, output) {
           theme_bw()+labs(y="log(count +1)")+
           theme(axis.text.x=element_blank(),
                 axis.ticks.x=element_blank(), legend.position = "top")
-        
+
         my_comparisons <- list( c("APPPS1+ Ctrl", "APPPS1+ Stroke"),
                                 c("APPPS1+ Ctrl",   "WT Ctrl"),
                                 c("APPPS1+ Ctrl", "WT Stroke"),
@@ -320,14 +316,14 @@ server <- function(input, output) {
           restab <- as.data.frame(comparison_all)[,c("gene","group1", "group2","p","p.adj")]
           restab$Celltype <- input$cluster
           restab <- restab[,c("gene", "Celltype","group1", "group2","p","p.adj")]
-          
+
         }
-        
+
         attach(restab)
         for(i in 1:nrow(restab)){
           idxbas=plotdata$gene==gene[i]&
             plotdata$Celltype==Celltype[i]
-          
+
           idxg1=idxbas& plotdata$corrGenotype_Treatment==group1[i]
           idxg2=idxbas& plotdata$corrGenotype_Treatment==group2[i]
           restab[i,"g1median"]=median(plotdata[idxg1, "counts"], na.rm=T)
@@ -342,17 +338,17 @@ server <- function(input, output) {
           theme_bw()+labs(y="log(count +1)")+
           theme(axis.text.x=element_blank(),
                 axis.ticks.x=element_blank(), legend.position = "top")
-        
+
         restab=data.frame(Exception="only one Condition Selected")
       }
     }
-    
+
     if(input$switch == "bycluster"){
       if(length(input$cluster)>1){
         comparison <- ggpubr:::compare_means(counts ~ Celltype,
                                              data = plotdata, method = "wilcox.test",
                                              group.by = c("gene"))
-        
+
         gp<-ggplot(plotdata, aes(x=Celltype, y=counts, fill=Celltype)) +
           geom_boxplot(outlier.shape = NA)+
           facet_grid(cols=vars(gene), rows=vars(corrGenotype_Treatment)) +
@@ -361,17 +357,17 @@ server <- function(input, output) {
           theme_bw()+labs(y="log(count +1)")+
           theme(axis.text.x=element_blank(),
                 axis.ticks.x=element_blank(), legend.position = "top")
-        
+
         tmp=comparison[comparison$gene==input$gene[1],c("group1", "group2")]
         tmp=tmp[order(tmp$group1,tmp$group2), ]
-        
+
         my_comparisons <- apply(tmp,1, c, simplify = F)
         gp <- gp + ggpubr:::stat_compare_means(comparison=my_comparisons,label = "p.adj",
                                              hide.ns = T, size = 3)
         n<-sum(filteridx)
-        
+
         if(length(input$treatment)>1){
-          
+
           comparison_all <- ggpubr:::compare_means(counts ~ Celltype,
                                                    data = plotdata, method = "wilcox.test",
                                                    group.by = c("gene","corrGenotype_Treatment"))
@@ -383,14 +379,14 @@ server <- function(input, output) {
           restab <- as.data.frame(comparison_all)[,c("gene", "group1", "group2","p","p.adj")]
           restab$corrGenotype_Treatment <- input$treatment
           restab <- restab[,c("gene", "corrGenotype_Treatment","group1", "group2","p","p.adj")]
-          
-          
+
+
         }
         attach(restab)
         for(i in 1:nrow(restab)){
           idxbas=plotdata$gene==gene[i]&
             plotdata$corrGenotype_Treatment==corrGenotype_Treatment[i]
-          
+
           idxg1=idxbas& plotdata$Celltype==group1[i]
           idxg2=idxbas& plotdata$Celltype==group2[i]
           restab[i,"g1median"]=median(plotdata[idxg1, "counts"], na.rm=T)
@@ -406,12 +402,12 @@ server <- function(input, output) {
           theme_bw()+labs(y="log(count +1)")+
           theme(axis.text.x=element_blank(),
                 axis.ticks.x=element_blank(), legend.position = "top")
-        
+
         restab=data.frame(Exception = "Only one Celltype selected")
       }
     }
-    
-    
+
+
     output$distPlot <- renderPlot(gp , height = min(c(2400, 500*n), na.rm=T))
     output$distTable <- DT::renderDataTable(
       DT::datatable(restab, extensions = "Buttons",
@@ -424,7 +420,7 @@ server <- function(input, output) {
                       ),dom = 'Blfrtip',
                       buttons = c('copy', 'csv', 'excel', 'pdf')
                     )))
-    
+
   })
   observe({
     trimmed_scaled <- function(vec){
@@ -442,14 +438,14 @@ server <- function(input, output) {
     rownames(tmp) = colnames(counts)
     plotdata <- tmp %>% as.data.frame()
     plotdata <- cbind(plotdata, metadata[rownames(plotdata),])
-    
+
     plotdata <- plotdata %>% reshape2::melt(., direction = "long",
                                             value.name = "counts",
                                             variable.name="gene",
                                             measure.vars = input$gene)
-    
+
     plotdata = plotdata[filteridx, ]
-    
+
     if(input$clustplot=="both"){
       a<-ggplot(plotdata, aes(x=as.numeric(tSNE_1),
                               y=as.numeric(tSNE_2))) +
@@ -467,18 +463,18 @@ server <- function(input, output) {
         geom_point(aes(colour=scale(counts)))+facet_wrap(vars(gene))+theme_bw()+
         ylab("UMAP_1")+xlab("UMAP_2")+ labs(colour="scaled reads")+scale_color_viridis(direction = -1)
     }
-    
+
     if(input$clustplot=="t-sne"){
       gp2<-ggplot(plotdata, aes(x=as.numeric(tSNE_1),
                               y=as.numeric(tSNE_2))) +
         geom_point(aes(colour=scale(counts)))+facet_wrap(vars(gene))+theme_bw()+
         ylab("tSNE_1")+xlab("tSNE_2")+ labs(colour="scaled reads")+scale_color_viridis(direction = -1)
-      
+
     }
-    
+
     output$MapPlots <- renderPlot(gp2, height = min(2400, 500*floor(sqrt(length(input$gene)))))
-    
-    
+
+
     if(input$clustplot=="both"){
       a<-ggplot(plotdata, aes(x=as.numeric(tSNE_1),
                               y=as.numeric(tSNE_2))) +
@@ -496,19 +492,19 @@ server <- function(input, output) {
         geom_point(aes(colour=get(input$condition)))+theme_bw()+
         ylab("UMAP_1")+xlab("UMAP_2")+ labs(colour=input$condition)
     }
-    
+
     if(input$clustplot=="t-sne"){
       p2<-ggplot(plotdata, aes(x=as.numeric(tSNE_1),
                                y=as.numeric(tSNE_2))) +
         geom_point(aes(colour=get(input$condition)))+theme_bw()+
         ylab("tSNE_1")+xlab("tSNE_2")+ labs(colour=input$condition)
-      
+
     }
-    
-    
+
+
     output$MapPlots_Feature <- renderPlot(p2)
-    
-    
+
+
   })
 }
 
