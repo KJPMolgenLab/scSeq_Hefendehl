192c4924224709627759b938c7d675fc451af6b6
achiocch <andreas.chiocchetti@web.de>
2026-03-20 09:00:04 +0000

    refactoring

--- wflowhelper.R
+++ wflowhelper.R
@@ -1,5 +1,12 @@
+
 library(workflowr)
 
 
-workflowr::wflow_build()
-workflowr::wflow_publish(c("./docs/*", "./analysis/*", "./code/*", "./output/*"))
+wflow_git_config(user.name = "achiocch", user.email = "andreas.chiocchetti@web.de", overwrite=T)
+wflow_start(directory = "/files/scSeq_Hefendehl/", existing = T)
+
+workflowr::wflow_build("./analysis/sc*.Rmd", delete_cache = TRUE)
+workflowr::wflow_publish(c("./docs/*", "./analysis/*.Rmd", "./code/*", "./output/Res_202602/*"), delete_cache = TRUE, message = "Refactoring")
+system("git push origin master")
+
+workflowr::wflow_publish(c("./analysis/*.Rmd"))
